b59f76fb8bf5a9ea167581fd05509f842db31517
Serhat KAYA <serhat_kaya@epam.com>
2023-10-17 10:32:05 +0300

    remove unnecessary comments

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -113,28 +113,6 @@ const serverlessConfiguration: AWS = {
           },
         ]
       ),
-      // [PRODUCT_TABLE_NAME]: {
-      //   Type: "AWS::DynamoDB::Table",
-      //   Properties: {
-      //     TableName: "products",
-      //     AttributeDefinitions: [
-      //       {
-      //         AttributeName: "id",
-      //         AttributeType: "S",
-      //       },
-      //     ],
-      //     KeySchema: [
-      //       {
-      //         AttributeName: "id",
-      //         KeyType: "HASH",
-      //       },
-      //     ],
-      //     ProvisionedThroughput: {
-      //       ReadCapacityUnits: 1,
-      //       WriteCapacityUnits: 1,
-      //     },
-      //   },
-      // },
     },
   },
 };
